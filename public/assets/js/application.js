c85546590c034cf3ef8f16f7c8d764f2bb1d019d
murilothebr <murilothebr>
2025-05-08 23:47:30 -0300

    feat: automaticaly remove the notifications / added icon to the browser bar

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -1,26 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const imagePreviewInput = document.getElementById("image_preview_input");
-  const preview = document.getElementById("image_preview");
-  const imagePreviewSubmit = document.getElementById("image_preview_submit");
+  function imagePreview() {
+    const imagePreviewInput = document.getElementById("image_preview_input");
+    const preview = document.getElementById("image_preview");
+    const imagePreviewSubmit = document.getElementById("image_preview_submit");
 
-  if (!(imagePreviewInput && preview)) return;
+    if (!(imagePreviewInput && preview)) return;
 
-  imagePreviewInput.style.display = "none";
-  imagePreviewSubmit.style.display = "none";
+    imagePreviewInput.style.display = "none";
+    imagePreviewSubmit.style.display = "none";
 
-  preview.addEventListener("click", function () {
-    imagePreviewInput.click();
-  });
+    preview.addEventListener("click", function () {
+      imagePreviewInput.click();
+    });
 
-  imagePreviewInput.addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        document.getElementById("image_preview").src = e.target.result;
-        imagePreviewSubmit.style.display = "block";
-      };
-      reader.readAsDataURL(file);
-    }
-  });
+    imagePreviewInput.addEventListener("change", function (event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          preview.src = e.target.result;
+          imagePreviewSubmit.style.display = "block";
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+  }
+
+  function hideFlashMessage() {
+    setTimeout(function () {
+      const flashMessage = document.getElementById("flash-message");
+      if (flashMessage) {
+        const alert = bootstrap.Alert.getOrCreateInstance(flashMessage);
+        alert.close();
+      }
+    }, 1500);
+  }
+
+  hideFlashMessage();
+  imagePreview();
 });
