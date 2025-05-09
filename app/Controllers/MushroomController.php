ed3588e421840a2b0f86929b3535c3812dea8cd4
murilothebr <murilothebr>
2025-05-08 23:30:32 -0300

    feat: login/logout working properly

---
+++ app/Controllers/MushroomController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use Core\Http\Controllers\Controller;
+use Core\Http\Request;
+use Lib\FlashMessage;
+
+class MushroomController extends Controller
+{
+    public function index(Request $request): void
+    {
+        $title = 'Mushroom Quizzes';
+
+        $paginator = null;
+        $mushrooms = [];
+
+        if ($request->acceptJson()) {
+            $this->renderJson('mushrooms/index', compact('paginator', 'mushrooms', 'title'));
+        } else {
+            $this->render('mushrooms/index', compact('paginator', 'mushrooms', 'title'));
+        }
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,20 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controllers\AuthenticationsController;
+use App\Controllers\MushroomController;
+
+
 use Core\Router\Route;
 
 // Authentication
-Route::get('/', [HomeController::class, 'index'])->name('root');
+Route::get('/login', [AuthenticationsController::class, 'new'])->name('users.login');
+Route::post('/login', [AuthenticationsController::class, 'authenticate'])->name('users.authenticate');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('root');
+    Route::get('/mushrooms', [MushroomController::class, 'index'])->name('mushrooms.index');
+
+    // Logout
+    Route::get('/logout', [AuthenticationsController::class, 'destroy'])->name('users.logout');
+});
